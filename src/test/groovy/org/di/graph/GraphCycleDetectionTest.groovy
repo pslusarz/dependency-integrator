90870bc3a9d04f6dfe3ce9a8f40cfe3a68404e91
Pawel Slusarz <p@sw7d.com>
2015-03-31 23:38:28 -0500

    test an extensive scenario

--- src/test/groovy/org/di/graph/GraphCycleDetectionTest.groovy
+++ src/test/groovy/org/di/graph/GraphCycleDetectionTest.groovy
@@ -142,4 +142,77 @@ class GraphCycleDetectionTest {
         assert tertiusCycle.size() == 2
         assert tertiusCycle.collect {it.name}.contains("quartus")
     }
+
+    @Test
+    void mixOfMultinodeCyclesAndTrees() {
+        SourceRepository sr = new SourceRepositoryForTesting({
+            project {
+                name = "lonely"
+            }
+            project {
+                name = "cycle4-1"
+                depends("cycle4-2")
+            }
+            project {
+                name = "cycle4-2"
+                depends("cycle4-3")
+            }
+            project {
+                name = "cycle4-3"
+                depends("cycle4-4")
+            }
+            project {
+                name = "cycle4-4"
+                depends("cycle4-1")
+            }
+
+            project {
+                name = "tree1"
+                depends("tree2")
+            }
+
+            project {
+                name = "tree2-1"
+                depends("tree2-2")
+            }
+
+            project {
+                name = "tree2-2"
+            }
+
+            project {
+                name = "tree2cycle-1"
+                depends("tree2cycle-2")
+            }
+
+            project {
+                name = "tree2cycle-2"
+                depends("cycle2-1")
+            }
+
+            project {
+                name = "cycle2-1"
+                depends("cycle2-2")
+            }
+
+            project {
+                name = "cycle2-2"
+                depends("cycle2-1")
+            }
+        })
+
+        Graph g = new Graph(sr)
+        g.initCycles()
+        assert g.cycles.size() == 2
+
+        def cycle4 = g.cycles.find {it.collect{it.name}.contains("cycle4-1")}
+        assert cycle4
+        assert cycle4.size() == 4
+        assert cycle4.collect {it.name}.containsAll(["cycle4-1","cycle4-2", "cycle4-3", "cycle4-4"])
+
+        def cycle2 = g.cycles.find {it.collect{it.name}.contains("cycle2-1")}
+        assert cycle2
+        assert cycle2.size() == 2
+        assert cycle2.collect {it.name}.containsAll(["cycle2-1","cycle2-2"])
+    }
 }
