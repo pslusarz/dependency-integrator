75e25366b075574b61b10308296c0f3320eb0db5
Paul Slusarz <PaulSlusarz@carfax.com>
2015-05-05 15:07:12 -0500

    extract methods

--- src/main/groovy/org/di/api/impl/Main.groovy
+++ src/main/groovy/org/di/api/impl/Main.groovy
@@ -1,6 +1,7 @@
 package org.di.api.impl
 
 import org.di.api.SourceRepository
+import org.di.engine.BuildRecord
 import org.di.engine.BuildRunner
 import org.di.graph.Graph
 import org.di.graph.visualization.GraphVizGenerator
@@ -9,36 +10,37 @@ import org.di.graph.visualization.GraphVizGenerator
 public class Main {
     public static void main(String... args) {
         SourceRepository repository = new CarfaxLibSourceRepository(localDir: new File("D:/hackathon"));
-       // repository.downloadAll()
-//        Graph g = new Graph(repository)
-//        g.initRank()
-//        g.cycles.each {
-//            println it
-//        }
-//        def gv = new GraphVizGenerator(graph: g)
-//                gv.generate()
-//        gv.reveal()
+        // repository.downloadAll()
+        //drawGraph(repository)
         def projects = repository.init()
-//        projects.each {
-//            println it.version
-//        }
-
-        long start = System.currentTimeMillis()
-        BuildRunner br = new BuildRunner(projectSources: projects)
-        br.start(5)
-        def results = br.completeBuildRecords
-        long stop = System.currentTimeMillis()
-        results.each {
-            println it.projectSource.name +" "+it.result
-        }
-
-        println "Total time (ms): "+(stop - start)
+        buildAll(projects)
 
 
     }
 
+    static buildAll(projects) {
+        long start = System.currentTimeMillis()
+        BuildRunner br = new BuildRunner(projectSources: projects)
+        br.start(8)
+        def results = br.completeBuildRecords
+        long stop = System.currentTimeMillis()
+        results.findAll { it.result == BuildRecord.BuildResult.Failed }.each {
+            println it.projectSource.name + " " + it.result
+        }
 
+        println "Total time (ms): " + (stop - start)
+    }
 
+    static drawGraph(repository) {
+        Graph g = new Graph(repository)
+        g.initRank()
+        g.cycles.each {
+            println it
+        }
+        def gv = new GraphVizGenerator(graph: g)
+        gv.generate()
+        gv.reveal()
+    }
 
 
 }
