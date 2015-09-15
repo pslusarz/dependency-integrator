fde46177e8738e77da8d8c8cdfffe1310324f74d
paulslusarz <paulslusarz@carfax.com>
2015-09-15 12:50:07 -0500

    Run gradle builds on mac

--- src/main/groovy/org/di/api/impl/carfax/util/Gradle.groovy
+++ src/main/groovy/org/di/api/impl/carfax/util/Gradle.groovy
@@ -7,7 +7,7 @@ class Gradle {
         File output = new File(System.getProperty("java.io.tmpdir"), "out-" + projectSource.projectDirectory.name +"-"+System.currentTimeMillis()+ ".txt")
         File buildFile = new File(projectSource.projectDirectory, "build.gradle")
         if (buildFile.exists()) {
-            Command.run("${projectSource.projectDirectory.absolutePath}\\gradlew.bat --build-file ${buildFile.absolutePath} --gradle-user-home ${projectSource.projectDirectory.absolutePath} ${command} >${output.absolutePath}")
+            Command.run("${projectSource.projectDirectory.absolutePath}\\gradlew{$Command.isWindows()?'.bat':''} --build-file ${buildFile.absolutePath} --gradle-user-home ${projectSource.projectDirectory.absolutePath} ${command} >${output.absolutePath}")
         } else {
             output << "Failure: build.gradle not found in "+projectSource.projectDirectory
         }
