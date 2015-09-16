7b9ed2f42ff8498764ecb7beeb2e1f2d2fb6f364
paulslusarz <paulslusarz@carfax.com>
2015-09-16 17:22:22 -0500

    trying massive fixes to get the jars to build

--- src/main/groovy/org/di/api/impl/carfax/Main.groovy
+++ src/main/groovy/org/di/api/impl/carfax/Main.groovy
@@ -3,6 +3,7 @@ package org.di.api.impl.carfax
 import com.fasterxml.jackson.databind.ObjectMapper
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
+import groovy.util.logging.Log
 import org.di.api.Dependency
 import org.di.api.ProjectSource
 import org.di.api.SourceRepository
@@ -18,7 +19,7 @@ import org.di.graph.Graph
 import org.di.graph.Node
 import org.di.graph.visualization.GraphVizGenerator
 
-
+@Log
 public class Main {
     public static void main(String... args) {
         SourceRepository repository = new CarfaxLibSourceRepository(localDir: new File("work/project-sources/"));
@@ -32,19 +33,84 @@ public class Main {
         // updateOneProject(repository, "dealerautoreports-commons")
         // demo(repository)
         //findAppropriateSubtree(repository)
-        updateOneAgain(repository, "servlet-extensions")
+        //updateOneAgain(repository, "vinalert-domain")
+        testAll(repository)
+        //updatePlugins(repository)
+    }
+//http\://resolver/gradle/carfax/gradle/2.6/gradle-2.6.zip
+    static void updatePlugins(repository) {
+        def projects = repository.init()
+        projects.each { CarfaxGradleProjectSource project ->
+
+            println "Updating "+project.name
+            updatePropertyInFile(new File(project.projectDirectory, "gradle.properties"), "carfaxPluginsVersion", "1.7.4")
+            updatePropertyInFile(new File(project.projectDirectory, "gradle/wrapper/gradle-wrapper.properties"), "distributionUrl", """http://resolver/gradle/carfax/gradle/2.6/gradle-2.6.zip""")
+        }
+    }
+
+    static updatePropertyInFile(File file, String propertyName, String propertyValue) {
+        Properties props = new Properties()
+        if (!file.exists()) {
+            log.warning("File ${file.absolutePath} does not exist. Cannot update ${propertyName} to ${propertyValue}")
+            return
+        }
+        props.load(file.newInputStream())
+        if (props[propertyName] == null) {
+            log.warning "Do not have property: ${propertyName} in ${file.absolutePath}"
+            return
+        }
+        props.store(new File(file.parentFile, "old-${System.currentTimeMillis()}-${file.name}").newOutputStream(), "backup before dependency-integrator")
+        props[propertyName] = propertyValue
+        props.store(file.newOutputStream(), "updated with dependency-integrator")
+
+    }
+
+    static void testAll(CarfaxLibSourceRepository repository) {
+        def projects = repository.init()
+        Graph graph = new Graph(projects)
+        graph.initRank()
+        long start = System.currentTimeMillis()
+        def results = new BuildRunner(projectSources: projects).start().completeBuildRecords
+        long length = System.currentTimeMillis() - start
+        println "RUNNNING ALL TEST TOOK: " + length
+
+        results.findAll { it.result == BuildRecord.BuildResult.Failed }.each { BuildRecord record ->
+            graph.nodes.find { it.name == record.projectSource.name }.buildFailed = true
+        }
+
+        def gv = new GraphVizGenerator(graph: graph)
+        gv.generate()
+        gv.reveal()
+
+        def failedProjectNodes = results.findAll {
+            it.result == BuildRecord.BuildResult.Failed
+        }.collect { BuildRecord record ->
+            graph.nodes.find { it.name == record.projectSource.name }
+        }
+
+        def passingProjectNodes = results.findAll {
+            it.result == BuildRecord.BuildResult.Passed
+        }.collect { BuildRecord record ->
+            graph.nodes.find { it.name == record.projectSource.name }
+        }
+        println "FAILED: " + failedProjectNodes.groupBy { it.rank }.sort()
+        println "PASSED: " + passingProjectNodes.groupBy { it.rank }.sort()
+
+
     }
 
     static void findAppropriateSubtree(CarfaxLibSourceRepository repository) {
         Graph g = new Graph(repository)
         def results = g.nodes.collectEntries { Node node ->
-            Graph dependents = new Graph(new SpanningTreeBuilder(world: g, treeRoot: node.name).connectedProjects.collect {it.projectSource})
+            Graph dependents = new Graph(new SpanningTreeBuilder(world: g, treeRoot: node.name).connectedProjects.collect {
+                it.projectSource
+            })
             int staleness = new StalenessCalculator(dependents).metric
-            println node.name + " "+dependents.nodes.size()+" "+staleness
-            [node.projectSource, [dependents: dependents ?:[], staleness: staleness]]
+            println node.name + " " + dependents.nodes.size() + " " + staleness
+            [node.projectSource, [dependents: dependents ?: [], staleness: staleness]]
         }
-        results.sort {it.value.dependents.nodes.size()}.each { k, v ->
-            println v.dependents.nodes.size() + "  " +k.name + " "+v.staleness
+        results.sort { it.value.dependents.nodes.size() }.each { k, v ->
+            println v.dependents.nodes.size() + "  " + k.name + " " + v.staleness
         }
 
     }
@@ -53,37 +119,39 @@ public class Main {
 
         Graph g = new Graph(repository)
 
-        Graph dependents = new Graph(new SpanningTreeBuilder(world: g, treeRoot: projectName).connectedProjects.collect {it.projectSource})
+        Graph dependents = new Graph(new SpanningTreeBuilder(world: g, treeRoot: projectName).connectedProjects.collect {
+            it.projectSource
+        })
         dependents.nodes.each { Node node ->
-          println "   "+node.name+ " "+node.projectSource.latestVersion
-          node.outgoing.each { Edge edge ->
-             println "    "+edge.to.name + "  "+edge.to.projectSource.latestVersion
-          }
+            println "   " + node.name + " " + node.projectSource.latestVersion
+            node.outgoing.each { Edge edge ->
+                println "    " + edge.to.name + "  " + edge.to.projectSource.latestVersion
+            }
         }
 
         StalenessCalculator calc = new StalenessCalculator(dependents)
-        println "STALENESS: "+calc.metric
+        println "STALENESS: " + calc.metric
         println "----- relative contribution per node:"
-        calc.nodeImpact.sort {- it.value}.each { node, impact ->
-            println node.name + " "+impact
+        calc.nodeImpact.sort { -it.value }.each { node, impact ->
+            println node.name + " " + impact
         }
         println "----- most stale build files and their total cost:"
-        calc.outdatedProjectImpact.sort {- it.value}.each { node, impact ->
-            println node.name + " "+impact
+        calc.outdatedProjectImpact.sort { -it.value }.each { node, impact ->
+            println node.name + " " + impact
         }
         println "======== UPDATING ========="
         Updater updater = new Updater(dependents)
         updater.update()
         println "===========DONE UPDATING======"
         StalenessCalculator calc2 = new StalenessCalculator(updater.graph)
-        println "STALENESS: "+calc2.metric
+        println "STALENESS: " + calc2.metric
         println "----- relative contribution per node:"
-        calc2.nodeImpact.sort {- it.value}.each { node, impact ->
-            println node.name + " "+impact
+        calc2.nodeImpact.sort { -it.value }.each { node, impact ->
+            println node.name + " " + impact
         }
         println "----- most stale build files and their total cost:"
-        calc2.outdatedProjectImpact.sort {- it.value}.each { node, impact ->
-            println node.name + " "+impact
+        calc2.outdatedProjectImpact.sort { -it.value }.each { node, impact ->
+            println node.name + " " + impact
         }
     }
 
@@ -109,28 +177,28 @@ public class Main {
         def projects = repository.init()
         Graph g = new Graph(projects)
         StalenessCalculator calc = new StalenessCalculator(g)
-        println "STALENESS: "+calc.metric
+        println "STALENESS: " + calc.metric
         println "----- relative contribution per node:"
-        calc.nodeImpact.sort {- it.value}.each { node, impact ->
-           println node.name + " "+impact
+        calc.nodeImpact.sort { -it.value }.each { node, impact ->
+            println node.name + " " + impact
         }
         println "----- most stale build files and their total cost:"
-        calc.outdatedProjectImpact.sort {- it.value}.each { node, impact ->
-            println node.name + " "+impact
+        calc.outdatedProjectImpact.sort { -it.value }.each { node, impact ->
+            println node.name + " " + impact
         }
         println "======== UPDATING ========="
         Updater updater = new Updater(g)
         updater.update()
         println "===========DONE UPDATING======"
         StalenessCalculator calc2 = new StalenessCalculator(updater.graph)
-        println "STALENESS: "+calc2.metric
+        println "STALENESS: " + calc2.metric
         println "----- relative contribution per node:"
-        calc2.nodeImpact.sort {- it.value}.each { node, impact ->
-            println node.name + " "+impact
+        calc2.nodeImpact.sort { -it.value }.each { node, impact ->
+            println node.name + " " + impact
         }
         println "----- most stale build files and their total cost:"
-        calc2.outdatedProjectImpact.sort {- it.value}.each { node, impact ->
-            println node.name + " "+impact
+        calc2.outdatedProjectImpact.sort { -it.value }.each { node, impact ->
+            println node.name + " " + impact
         }
     }
 
@@ -145,32 +213,32 @@ public class Main {
         def versionCounts = new PastProjectSources(localDir: new File('src/test/resources')).referencedVersionCounts()
         def projects = repository.init()
         projects.each { project ->
-           project.versions.each {version ->
-               if (versionCounts[project.name][version.toString()] == null) {
-                   versionCounts[project.name][version.toString()] = 0
-               }
-           }
+            project.versions.each { version ->
+                if (versionCounts[project.name][version.toString()] == null) {
+                    versionCounts[project.name][version.toString()] = 0
+                }
+            }
         }
         int referenced = 0
         int unreferenced = 0
-        versionCounts.each {project, versions ->
-println project
-            println "   "+versions
-            if (versions.find {k, v -> v > 0}) {
-                int haveRefs = versions.findAll {k, v -> v > 0}.size()
-               referenced +=  haveRefs
-               unreferenced += versions.size() - haveRefs
+        versionCounts.each { project, versions ->
+            println project
+            println "   " + versions
+            if (versions.find { k, v -> v > 0 }) {
+                int haveRefs = versions.findAll { k, v -> v > 0 }.size()
+                referenced += haveRefs
+                unreferenced += versions.size() - haveRefs
             }
         }
 
-        println "referenced: "+referenced
-        println "unreferenced: "+unreferenced
+        println "referenced: " + referenced
+        println "unreferenced: " + unreferenced
     }
 
     static displayVersions(SourceRepository repository) {
         List<ProjectSource> projectSources = repository.init()
-        projectSources.sort {-it.versions.size()}.each {
-            println it.name + "  "+it.versions.size()
+        projectSources.sort { -it.versions.size() }.each {
+            println it.name + "  " + it.versions.size()
         }
     }
 
--- src/main/groovy/org/di/engine/BuildRunner.groovy
+++ src/main/groovy/org/di/engine/BuildRunner.groovy
@@ -18,6 +18,7 @@ class BuildRunner {
           record.startEpoch = System.currentTimeMillis()
           executor.execute(new BuildRun(buildRecord: record))
         }
+        return this
     }
 
 
