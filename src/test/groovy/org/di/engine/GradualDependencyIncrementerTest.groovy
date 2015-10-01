6c99b6d7b7893c4e79432b8f75020303e7ad288a
paulslusarz <paulslusarz@carfax.com>
2015-10-01 15:39:06 -0500

    I have no idea why these tests are passing

--- src/test/groovy/org/di/engine/GradualDependencyIncrementerTest.groovy
+++ src/test/groovy/org/di/engine/GradualDependencyIncrementerTest.groovy
@@ -70,9 +70,119 @@ class GradualDependencyIncrementerTest {
 
         di.increment()
         di.rollback()
-        def currentDependency = g.rebuild().node("one").outgoing("current").dependency
-        assert currentDependency.version == new VersionForTesting(value: 1) //node
+
+        assert g.rebuild().node("one").outgoing("current").dependency.version == new VersionForTesting(value: 1) //node
         assert g.rebuild().node("one").projectSource.dependencies.find {it.projectSourceName == "current"}.version == new VersionForTesting(value: 1) //project associated with node
+    }
+
+    @Test
+    void testIncrementTwoDependenciesGradually() {
+        Graph g = new Graph(new SourceRepositoryForTesting({
+            project {
+                name = "one"
+                depends ("left", 1)
+                depends ("right", 4)
+            }
+            project {
+                name = "left"
+                version = 2
+            }
+
+            project {
+                name = "right"
+                version = 5
+            }
+        }))
+        def di = new GradualDependencyIncrementer(node: g.node("one"))
+
+        di.increment()
+        di.increment()
+
+        g.rebuild().node("one").outgoing("left").dependency == new VersionForTesting(value: 2)
+        g.rebuild().node("one").outgoing("right").dependency == new VersionForTesting(value: 5)
+    }
+
+    @Test
+    void bypassFailedTwoDependencies() {
+        Graph g = new Graph(new SourceRepositoryForTesting({
+            project {
+                name = "one"
+                depends ("left", 1)
+                depends ("right", 4)
+            }
+            project {
+                name = "left"
+                version = 2
+            }
+
+            project {
+                name = "right"
+                version = 5
+            }
+        }))
+        def di = new GradualDependencyIncrementer(node: g.node("one"))
+
+        di.increment()
+        di.rollback()
+        di.increment()
+
+        g.rebuild().node("one").outgoing("left").dependency == new VersionForTesting(value: 1) //rolled back
+        g.rebuild().node("one").outgoing("right").dependency == new VersionForTesting(value: 5)
+    }
+
+    @Test
+    void bypassFailedSingleDependency() {
+        Graph g = new Graph(new SourceRepositoryForTesting({
+            project {
+                name = "one"
+                depends ("two", 1)
+            }
+            project {
+                name = "two"
+                version = 5
+            }
+
+        }))
+        def di = new GradualDependencyIncrementer(node: g.node("one"))
+
+        di.increment()
+        di.rollback() //failed on version 2
+        di.increment()
+
+        g.rebuild().node("one").outgoing("two").dependency == new VersionForTesting(value: 3)
+
+        di.increment()
+        g.rebuild().node("one").outgoing("two").dependency == new VersionForTesting(value: 4)
+        di.rollback() //failed on 4
+        di.increment()
+        g.rebuild().node("one").outgoing("two").dependency == new VersionForTesting(value: 5)
 
     }
+
+    @Test
+    void rollbackToLastPassingIfContinuesToFail() {
+        Graph g = new Graph(new SourceRepositoryForTesting({
+            project {
+                name = "one"
+                depends ("two", 1)
+            }
+            project {
+                name = "two"
+                version = 5
+            }
+
+        }))
+        def di = new GradualDependencyIncrementer(node: g.node("one"))
+
+        di.increment() //last passing version: 2
+        di.increment() //try version 3
+        di.rollback()  //3 fails
+        di.increment() //now try 4
+        di.rollback() //4 fails
+        di.increment() //try 5
+        g.rebuild().node("one").outgoing("two").dependency == new VersionForTesting(value: 5)
+        di.rollback() //5 fails
+        g.rebuild().node("one").outgoing("two").dependency == new VersionForTesting(value: 2)
+    }
+
 }
