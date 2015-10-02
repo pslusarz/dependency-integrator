a8cca98b4360b4a48836e4b970c099bc8656c6bb
paulslusarz <paulslusarz@carfax.com>
2015-10-02 13:16:03 -0500

    still failing, but with code that has a chance of working

--- src/main/groovy/org/di/engine/GradualDependencyIncrementer.groovy
+++ src/main/groovy/org/di/engine/GradualDependencyIncrementer.groovy
@@ -1,5 +1,6 @@
 package org.di.engine
 
+import org.di.api.ProjectSource
 import org.di.api.Version
 import org.di.graph.Edge
 
@@ -10,26 +11,71 @@ class GradualDependencyIncrementer extends DependencyIncrementer {
 
     @Override
     boolean increment() {
+        boolean result = false
         Edge staleEdge = node.outgoing.find { it.isStale() }
+
         if (staleEdge != null) {
+            if (passingVersions[staleEdge].size() == 0) { //assume initial version is passing so it can be rolled back
+                passingVersions[staleEdge] << staleEdge.dependency.version
+            }
             if (originalVersions[staleEdge] != null) {
                 passingVersions[staleEdge] << originalVersions[staleEdge]
             }
             originalVersions[staleEdge] = staleEdge.dependency.version
-            staleEdge.setDependencyVersion(staleEdge.to.projectSource.versions[staleEdge.to.projectSource.versions.indexOf(staleEdge.dependency.version) + 1])
+            Version nextVersion = getNextVersion(staleEdge.to.projectSource, max(lastOrNull(passingVersions[staleEdge]), lastOrNull(failingVersions[staleEdge])))
+            if (nextVersion == null) {
+                result = false
+            } else {
+                staleEdge.setDependencyVersion(nextVersion)
+                result = true
+            }
             return true
-        } else {
-            return false
         }
+        return result
     }
 
     @Override
     void rollback() {
         originalVersions.each { Edge edge, Version version ->
             failingVersions[edge] << edge.dependency.version
-            edge.setDependencyVersion(version)
+            edge.setDependencyVersion(passingVersions[edge].last())
 
         }
         originalVersions.clear()
+    }
+
+    Version getNextVersion(ProjectSource projectSource, Version current) {
+       int currentIndex = projectSource.versions.indexOf(current)
+        if (currentIndex == -1) {
+           return projectSource.versions.first()
+        }
+        if (currentIndex == projectSource.versions.size() -1) {
+          return null
+        }
+        return projectSource.versions[currentIndex+1]
+    }
+
+    Version max(Version first, second) {
+        Version result
+        if (first == null) {
+            result = second
+        } else
+        if (second == null) {
+            result = first
+        } else
+        if (first.after(second)) {
+            result = first
+        } else {
+            result = second
+        }
+        return result
+    }
+
+    Version lastOrNull(List<Version> list) {
+        if (list.size() == 0) {
+            return null
+        } else {
+            return list.last()
+        }
     }
 }
